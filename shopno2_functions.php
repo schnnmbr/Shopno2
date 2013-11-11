446916ab0ab277a0749a57107ba754073302dbb3
schnnmbr <sachingopalakrishnan@gmail.com>
2013-11-11 19:41:38 +0530

    Roll back previous commit

--- shopno2_functions.php
+++ shopno2_functions.php
@@ -68,14 +68,7 @@ if (basename($_SERVER['PHP_SELF']) == basename (__FILE__)) {
 */
 
 
-// REMOVE THE WORDPRESS UPDATE NOTIFICATION FOR ALL USERS EXCEPT SYSADMIN
-       global $user_login;
-       get_currentuserinfo();
-       if (!current_user_can('update_plugins')) { // checks to see if current user can update plugins
-        add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-        add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
-       }
-       
+
 // Obscure login screen error messages
 function shopno2_login_obscure(){ return 'Wrong Username/Password Combination';}
 add_filter( 'login_errors', 'shopno2_login_obscure' );
